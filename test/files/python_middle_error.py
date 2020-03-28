713dc6f7847e2848951520bb306f8e22a8a1e37a
Fabian Homborg <FHomborg@gmail.com>
2020-03-28 14:16:10 +0100

    Add context display
    This keeps the last 5 (configurable, via --before) lines in a buffer
    and displays them, together with the failing line, the failing input
    and the "after" lines.
    
    It looks like
    
      Context:
        fine so far
        now stdout is broken <= does not match 'things should be cool'
    
      when running command:
        /usr/bin/python python_out_vs_err.py
    
    with coloring for the lines.

---
+++ test/files/python_middle_error.py
@@ -0,0 +1,20 @@
+# RUN: /usr/bin/python %s
+
+import sys
+
+sys.stderr.write("GAMMA1\n")
+# CHECKERR: GAMMA1
+sys.stderr.write("GAMMA2\n")
+# CHECKERR: GAMMA2
+sys.stderr.write("GAMMA3\n")
+# CHECKERR: BETA
+sys.stderr.write("GAMMA4\n")
+# CHECKERR: GAMMA4
+sys.stderr.write("GAMMA5\n")
+# CHECKERR: GAMMA5
+sys.stderr.write("GAMMA6\n")
+# CHECKERR: GAMMA6
+sys.stderr.write("GAMMA7\n")
+# CHECKERR: GAMMA7
+sys.stderr.write("GAMMA8\n")
+# CHECKERR: GAMMA8
