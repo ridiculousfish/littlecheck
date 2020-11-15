b9c24a3e818eb2d67e46655d5a616b54e1c88314
Fabian Homborg <FHomborg@gmail.com>
2020-11-15 22:01:17 +0100

    Create a diff between the lines and the checks
    This creates output like
    
    ```
    Failure in test/files/python_missing_output.py:
    
      The CHECK on line 8 wants:
        NotPrinted
    
      which failed to match line stdout:2:
        Second
    
      Context:
        First
         <= nothing to match CHECK 'NotPrinted' on line 8
        Second
        Third
        1 <= no check matches this, previous check on line 13
        2
        3
        4
        [...] from line 34 (stdout:14):
        6
        6
        6
        Fourth <= does not match CHECK '{{\\d}}' on line 40
    
      when running command:
        /usr/bin/python test/files/python_missing_output.py
    ```
    
    (with color).
    
    Instead of just listing the first error (CHECK on line 8 doesn't have a match),
    we manage to list the other errors as well, and we also figure out that there's a line *missing*,
    instead of chalking it up to "Second" not matching "NotPrinted", or "no more CHECKS left".
    
    Really the breakthrough here is that we go through the entire
    list, and if a regex matches *add the literal text* to the list. That
    way we can use diffing libs because now it's all just strings.

--- littlecheck/littlecheck.py
+++ littlecheck/littlecheck.py
@@ -13,6 +13,11 @@ import re
 import shlex
 import subprocess
 import sys
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
+from difflib import SequenceMatcher
 
 # Directives can occur at the beginning of a line, or anywhere in a line that does not start with #.
 COMMENT_RE = r'^(?:[^#].*)?#\s*'
@@ -128,9 +133,11 @@ class Line(object):
     def is_empty_space(self):
         return not self.text or self.text.isspace()
 
-    def escaped_text(self):
-        return escape_string(self.text.rstrip("\n"))
-
+    def escaped_text(self, for_formatting=False):
+        ret = escape_string(self.text.rstrip("\n"))
+        if for_formatting:
+            ret = ret.replace("{", "{{").replace("}", "}}")
+        return ret
 
 class RunCmd(object):
     """ A command to run on a given Checker.
@@ -151,11 +158,14 @@ class RunCmd(object):
 
 
 class TestFailure(object):
-    def __init__(self, line, check, testrun):
+    def __init__(self, line, check, testrun, diff=None, lines=[], checks=[]):
         self.line = line
         self.check = check
         self.testrun = testrun
         self.error_annotation_lines = None
+        self.diff = diff
+        self.lines = lines
+        self.checks = checks
 
     def message(self):
         fields = self.testrun.config.colors()
@@ -213,6 +223,50 @@ class TestFailure(object):
                 "  additional output on stderr:{error_annotation_lineno}:",
                 "    {BOLD}{error_annotation}{RESET}",
             ]
+        if self.diff:
+            fmtstrs += ["  Context:"]
+            lasthi = 0
+            lastcheckline = None
+            for d in self.diff.get_grouped_opcodes():
+                for op, alo, ahi, blo, bhi in d:
+                    color="{BOLD}"
+                    if op == 'replace' or op == 'delete':
+                        color="{RED}"
+                    # We got a new chunk, so we print a marker.
+                    if alo > lasthi:
+                        fmtstrs += [
+                            "    [...] from line " + str(self.checks[blo].line.number)
+                            + " (" + self.lines[alo].file + ":" + str(self.lines[alo].number) + "):"
+                        ]
+                    lasthi = ahi
+
+                    # We print one "no more checks" after the last check and then skip any markers
+                    lastcheck = False
+                    for a, b in zip_longest(self.lines[alo:ahi], self.checks[blo:bhi]):
+                        # Clean up strings for use in a format string - double up the curlies.
+                        astr = color + a.escaped_text(for_formatting=True) + "{RESET}" if a else ""
+                        if b:
+                            bstr = "'{BLUE}" + b.line.escaped_text(for_formatting=True) + "{RESET}'" + " on line " + str(b.line.number)
+                            lastcheckline = b.line.number
+
+                        if op == 'equal':
+                            fmtstrs += ["    " + astr]
+                        elif b and a:
+                            fmtstrs += ["    " + astr + " <= does not match " + b.type + " " + bstr]
+                        elif b:
+                            fmtstrs += ["    " + astr + " <= nothing to match " + b.type + " " + bstr]
+                        elif not b:
+                            string = "    " + astr
+                            if bhi == len(self.checks):
+                                if not lastcheck:
+                                    string += " <= no more checks"
+                                    lastcheck = True
+                            elif lastcheckline is not None:
+                                string += " <= no check matches this, previous check on line " + str(lastcheckline)
+                            else:
+                                string += " <= no check matches"
+                            fmtstrs.append(string)
+            fmtstrs.append("")
         fmtstrs += ["  when running command:", "    {subbed_command}"]
         return "\n".join(fmtstrs).format(**fields)
 
@@ -256,36 +310,71 @@ class TestRun(object):
         # Reverse our lines and checks so we can pop off the end.
         lineq = lines[::-1]
         checkq = checks[::-1]
+        usedlines = []
+        usedchecks = []
+        text1 = []
+        text2 = []
+        mismatches = []
         while lineq and checkq:
             line = lineq[-1]
             check = checkq[-1]
             if check.regex.match(line.text):
                 # This line matched this checker, continue on.
+                text1.append(line.escaped_text())
+                usedlines.append(line)
+                text2.append(line.escaped_text())
+                usedchecks.append(check)
                 lineq.pop()
                 checkq.pop()
             elif line.is_empty_space():
                 # Skip all whitespace input lines.
                 lineq.pop()
             else:
+                text1.append(line.escaped_text())
+                usedlines.append(line)
+                # HACK: Theoretically it's possible that
+                # the line is the same as the CHECK regex but doesn't match
+                # (e.g. both are `\s+` or something).
+                # Since we only need this for the SequenceMatcher to *compare*,
+                # we give it a fake non-matching check in those cases.
+                etext = check.line.escaped_text()
+                if etext != line.escaped_text():
+                    text2.append(etext)
+                else:
+                    text2.append(" " + etext)
+
+                usedchecks.append(check)
+                mismatches.append((line, check))
                 # Failed to match.
                 lineq.pop()
-                line.text = line.escaped_text() + "\n"
-                # Add context, ignoring empty lines.
-                return TestFailure(
-                    line,
-                    check,
-                    self,
-                )
-        # Drain empties.
+                checkq.pop()
+
+        # Drain empties
         while lineq and lineq[-1].is_empty_space():
             lineq.pop()
-        # If there's still lines or checkers, we have a failure.
+
+        # Store the remaining lines for the diff
+        for i in lineq[::-1]:
+            if not i.is_empty_space():
+                text1.append(i.escaped_text())
+                usedlines.append(i)
+        # Store remaining checks for the diff
+        for i in checkq[::-1]:
+            text2.append(i.line.escaped_text())
+            usedchecks.append(i)
+
+        # Do a SequenceMatch! This gives us a diff-like thing.
+        diff = SequenceMatcher(a=text1, b=text2)
+        # If there's a mismatch or still lines or checkers, we have a failure.
         # Otherwise it's success.
-        if lineq:
-            return TestFailure(lineq[-1], None, self)
+        if mismatches:
+            return TestFailure(mismatches[0][0], mismatches[0][1], self, diff=diff, lines=usedlines, checks=usedchecks)
+        elif lineq:
+            return TestFailure(lineq[-1], None, self, diff=diff, lines=usedlines, checks=usedchecks)
         elif checkq:
-            return TestFailure(None, checkq[-1], self)
+            return TestFailure(None, checkq[-1], self, diff=diff, lines=usedlines, checks=usedchecks)
         else:
+            # Success!
             return None
 
     def run(self):
--- test/files/python_missing_output.py
+++ test/files/python_missing_output.py
@@ -6,3 +6,37 @@ print("First")
 # CHECK: First
 
 # CHECK: NotPrinted
+
+print("Second")
+# CHECK: Second
+print("Third")
+# CHECK: Third
+
+print("1")
+# CHECK: {{\d}}
+print("2")
+# CHECK: {{\d}}
+print("3")
+# CHECK: {{\d}}
+print("4")
+# CHECK: {{\d}}
+print("5")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+print("6")
+# CHECK: {{\d}}
+
+print("Fourth")
